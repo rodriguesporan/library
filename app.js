97309f5233ef730764ce907a8abadd6fe9000345
rodriguesporan <rodriguesporan@gmail.com>
2019-05-27 09:08:36 -0300

    Criado um Router para as páginas de Books

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -13,5 +14,8 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+bookRouter.route('/').get((req, res) => res.send('Books'));
+bookRouter.route('/single').get((req, res) => res.send('Single Book Page'));
+app.use('/books', bookRouter);
 app.get('/', (req, res) => res.render('index', { title: 'Library', nav: [{ title: 'Authors', link: '/authors' }, { title: 'Books', link: '/books' }] }));
 app.listen(port, () => debug(`Server listenning on port ${chalk.green(port)}`));
