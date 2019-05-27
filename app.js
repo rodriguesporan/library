1c1d1d4ac3af2d8cda6d4786739fadf2d31bc723
rodriguesporan <rodriguesporan@gmail.com>
2019-05-27 09:04:21 -0300

    Criada a navegação do menu

--- app.js
+++ app.js
@@ -13,6 +13,5 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
-app.get('/', (req, res) => res.render('index', { title: 'Library', list: ['a', 'b'] }));
+app.get('/', (req, res) => res.render('index', { title: 'Library', nav: [{ title: 'Authors', link: '/authors' }, { title: 'Books', link: '/books' }] }));
 app.listen(port, () => debug(`Server listenning on port ${chalk.green(port)}`));
