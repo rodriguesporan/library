d70af530b8ae74cb2878fd4f27a30f19d3a7a3b2
rodriguesporan <rodriguesporan@gmail.com>
2019-05-26 21:58:54 -0300

    Adicionado o Template Engine Ejs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
-app.get('/', (req, res) => res.render('index', { title: 'Pug Index', list: ['a', 'b'] }));
+app.get('/', (req, res) => res.render('index', { title: 'Library', list: ['a', 'b'] }));
 app.listen(port, () => debug(`Server listenning on port ${chalk.green(port)}`));
