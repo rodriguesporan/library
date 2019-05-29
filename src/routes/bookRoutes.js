30f8e2ab2c89d22a35626f5ab6f96c7b037a27d3
rodriguesporan <rodriguesporan@gmail.com>
2019-05-29 15:01:47 -0300

    Usando async await functions para lidar com as consultas do banco de dados

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,33 +1,24 @@
 const express = require('express');
-const debug = require('debug')('app:bookRoutes');
+// const debug = require('debug')('app:bookRoutes');
 const sql = require('mssql');
 
 const bookRouter = express.Router();
 const route = (nav) => {
-  const books = [{
-    title: 'A preparação do ator', genre: 'Artes cenicas', author: 'Constantin Stanislavski', read: true,
-  },
-  {
-    title: 'Essencialismo', genre: 'Auto ajuda', author: 'Greg McKeown', read: true,
-  },
-  {
-    title: 'Feminismo em comum', genre: 'Feminismo', author: 'Marcia Tiburi', read: true,
-  },
-  {
-    title: 'Como vejo o mundo', genre: 'Filosofia', author: 'Albert Einstein', read: false,
-  },
-  ];
   bookRouter.route('/').get((req, res) => {
-    const request = new sql.Request();
-    request.query('SELECT * FROM books').then((result) => {
-      const { recordset } = result;
+    (async function query() {
+      const request = new sql.Request();
+      const { recordset } = await request.query('SELECT * FROM books');
       res.render('bookListView', { title: 'Library', nav, books: recordset });
-      debug(recordset);
-    });
+    }());
   });
   bookRouter.route('/:id').get((req, res) => {
     const { id } = req.params;
-    res.render('bookView', { title: 'Library', nav, book: books[id] });
+    (async function query() {
+      const request = new sql.Request();
+      const { recordset } = await request.query(`SELECT * FROM books WHERE id = '${id}'`);
+      const [book] = recordset;
+      res.render('bookView', { title: 'Library', nav, book });
+    }());
   });
   return bookRouter;
 };
